64f47a30087dd6fd4a65873efd8c8250d5c89723
Lucio Dias <luciodiass@gmail.com>
2025-10-17 17:13:05 -0300

    testGetPreco

--- br/fastfood/teste/TestBurger.java
+++ br/fastfood/teste/TestBurger.java
@@ -12,11 +12,21 @@ public class TestBurger {
         // Arrange
         String expectedNome = "Cheeseburger";
         Burger burger = new Burger(expectedNome, 10.50);
-
         // Act
         String actualNome = burger.getNome();
-
         // Assert
         assertEquals(expectedNome, actualNome, "The name of the burger should match the expected value.");
     }
+
+    @Test
+    public void testGetPreco() {
+        // Arrange
+        double expectedPreco = 10.50;
+        Burger burger = new Burger("Cheeseburger", expectedPreco);
+        // Act
+        double actualPreco = burger.getPreco();
+        // Assert
+        assertEquals(expectedPreco, actualPreco, "The price of the burger should match the expected value.");
+    }
+
 }
